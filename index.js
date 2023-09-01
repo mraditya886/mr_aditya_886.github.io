535bf7940c82e37053d4dc49e6143be85ef0f3d8
Aditya Kumar Singh <128143385+mraditya886@users.noreply.github.com>
2023-09-01 14:47:15 -0600

    mr_aditya_886

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("registration-form").addEventListener("submit", function(event) {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+  
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      event.preventDefault(); // Prevent form submission
+    }
+  });
+  
